e6a27d63adb6663d99b3d1d9f67eb6e50319a481
Osazee Isere <oz1.concepts@gmail.com>
2021-01-07 05:36:09 +0100

    Designing the welcome page

---
+++ OnlineSimpleInventory/content.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class content : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        top_heading.InnerText = "Online Inventory System";
+    }
+
+    protected void change_title_Click(object sender, EventArgs e)
+    {
+        top_heading.InnerText = "Online Inventory System by Isere Osaze";
+    }
+}
